b1792435d896f6f27e5c2d1e53391aae4b1f69bd
Alfred <arista21dominguez@gmail.com>
2025-04-11 13:24:00 -0600

    enrrutando dash y not found

--- front-employee/src/App.tsx
+++ front-employee/src/App.tsx
@@ -1,10 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./pages/header/Header";
+import Dashboard from "./pages/dashboard/dashboard";
+import NoMatch from "./pages/noMatch/noMatch";
 
 function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
     </>
   );
 }
