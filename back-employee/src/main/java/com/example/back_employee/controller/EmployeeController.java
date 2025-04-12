fb755092cd7d9962be0bc8cc6ed79ea69c24841d
Alfred <arista21dominguez@gmail.com>
2025-04-11 22:16:36 -0600

    agregando endpoint para eliminar employee

--- back-employee/src/main/java/com/example/back_employee/controller/EmployeeController.java
+++ back-employee/src/main/java/com/example/back_employee/controller/EmployeeController.java
@@ -2,9 +2,13 @@ package com.example.back_employee.controller;
 
 import com.example.back_employee.entity.Employee;
 import com.example.back_employee.service.EmployeeService;
+import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -33,5 +37,17 @@ public class EmployeeController
     public List<Employee> getAllEmployees()
     {
         return employeeService.getAllEmployees();
+    }
+    
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> deleteEmployee(@PathVariable Long id)
+    {
+        try{
+            employeeService.deleteEmployee(id);
+            return new ResponseEntity<>("Employee with ID: " + id + " deleted successfuly", HttpStatus.OK);
+        }catch(EntityNotFoundException e)
+        {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
     }
 }
--- back-employee/src/main/java/com/example/back_employee/service/EmployeeService.java
+++ back-employee/src/main/java/com/example/back_employee/service/EmployeeService.java
@@ -3,6 +3,7 @@ package com.example.back_employee.service;
 import com.example.back_employee.repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
 import com.example.back_employee.entity.Employee;
+import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -25,5 +26,13 @@ public class EmployeeService
     public List<Employee> getAllEmployees()
     {
         return employeeRepository.findAll();
+    }
+    
+    public void deleteEmployee(Long id)
+    {
+        if(!employeeRepository.existsById(id))
+            throw new EntityNotFoundException("Employee with ID : " + id + " not found");
+        
+        employeeRepository.deleteById(id);
     }
 }
